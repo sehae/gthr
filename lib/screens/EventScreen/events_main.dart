1bf282a00d83863c915ca848dedd5977969d5c48
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-14 20:57:25 +0800

    just updated

--- lib/screens/EventScreen/events_main.dart
+++ lib/screens/EventScreen/events_main.dart
@@ -12,7 +12,21 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Events'),
+          title: Text(
+              'Events',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              icon: Image.asset('assets/gthr_LogoALT.png'),
+              onPressed: (){
+              },
+            )
+          ],
         ),
         drawer: SizedBox(
           width: MediaQuery.of(context).size.width * 0.5,
@@ -32,10 +46,14 @@ class MyApp extends StatelessWidget {
                         leading: Icon(
                           Icons.home,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Home',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
                         onTap: (){
                           'Navigator.of(context).push('
@@ -47,50 +65,70 @@ class MyApp extends StatelessWidget {
                         leading: Icon(
                           Icons.event,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Events',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                       ListTile(
                         leading: Icon(
                           Icons.chat_bubble,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Chats',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
                       ),
                       ListTile(
                         leading: Icon(
                           Icons.people_alt_rounded,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Friends',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
                       ),
                       ListTile(
                         leading: Icon(
                           Icons.person,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Profile',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
                       ),
                       ListTile(
                         leading: Icon(
                           Icons.settings,
                           color: Color(0xFF1E5720),
+                          size: 32,
                         ),
                         title: Text(
                           'Settings',
-                          style: TextStyle(fontSize: 18),
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                          ),
                         ),
                       ),
                     ],
@@ -100,32 +138,113 @@ class MyApp extends StatelessWidget {
                   leading: Icon(
                     Icons.logout,
                     color: Color(0xFF1E5720),
+                    size: 32,
                   ),
                   title: Text(
-                    'Logout',
-                    style: TextStyle(fontSize: 18),
+                    'Sign-out',
+                    style: TextStyle(
+                        fontSize: 20,
+                    fontWeight: FontWeight.bold
+                    ),
                   ),
                 ),
               ],
             ),
           ),
         ),
-        body: content(),
+        body: Content(),
       ),
     );
   }
 }
 
-class content extends StatelessWidget {
+class Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 50,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text('Item $index'),
-        );
-      },
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                Color(0xFF1E7251).withOpacity(0.4),
+                BlendMode.srcATop,
+              ),
+              child: Image.asset(
+                'assets/test.png',
+                width: 350,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: FittedBox(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                      },
+                      icon: Icon(Icons.star),
+                      label: Text('Interested'),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFDD09)),
+                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: FittedBox(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                      },
+                      icon: Icon(Icons.check),
+                      label: Text('Going'),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
+                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Expanded(
+                  child: FittedBox(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                      },
+                      icon: Icon(Icons.close),
+                      label: Text('Not Going'),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
+                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
