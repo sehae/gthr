fb8cef7073abd9b7f531e783c74f0a7de97f39ac
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-14 22:33:00 +0800

    Update events_main.dart

--- lib/screens/EventScreen/events_main.dart
+++ lib/screens/EventScreen/events_main.dart
@@ -161,88 +161,267 @@ class MyApp extends StatelessWidget {
 class Content extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                Color(0xFF1E7251).withOpacity(0.4),
-                BlendMode.srcATop,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 8,
+                      offset: Offset(0, 8),
+                    ),
+                  ],
+                ),
+                child: ColorFiltered(
+                  colorFilter: ColorFilter.mode(
+                    Color(0xFF1E7251).withOpacity(0.4),
+                    BlendMode.srcATop,
+                  ),
+                  child: Image.asset(
+                    'assets/test.png',
+                    width: 350,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              child: Image.asset(
-                'assets/test.png',
-                width: 350,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: FittedBox(
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                      },
-                      icon: Icon(Icons.star),
-                      label: Text('Interested'),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                    child: FittedBox(
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                        },
+                        icon: Icon(Icons.star),
+                        label: Text('Interested'),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
                           ),
+                          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFDD09)),
+                          foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
                         ),
-                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFDD09)),
-                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: FittedBox(
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                      },
-                      icon: Icon(Icons.check),
-                      label: Text('Going'),
+                  SizedBox(width: 10),
+                  Container(
+                    width: 100,
+                    child: Expanded(
+                      child: FittedBox(
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                          },
+                          icon: Icon(Icons.check),
+                          label: Text('Going'),
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
+                            foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: FittedBox(
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                        },
+                        icon: Icon(Icons.close),
+                        label: Text('Not Going'),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
+                          foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Code Revibe',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      Text(
+                        'By ACM',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '9 People are Interested',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            size: 20,
+                            color: Color(0xFF2C2C30),
+                          ),
+                          SizedBox(width: 5,),
+                          Text(
+                            'Building 9',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.lock,
+                            size: 20,
+                            color: Color(0xFF2C2C30),
+                          ),
+                          SizedBox(width: 5,),
+                          Text(
+                            'Open for TIPQC Students',
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 80,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD9D9D9),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                        child: Center(
+                          child: Text(
+                            'Feb 26',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFF2C2C30),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFD9D9D9),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                        child: Center(
+                          child: Text(
+                            '10:30am',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color(0xFF2C2C30),
+                              fontSize: 10,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('About'),
                       style: ButtonStyle(
                         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10.0),
                           ),
                         ),
+                        minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 30)),
+                        maximumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)),
+                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF787878)),
+                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFFFFF)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                      },
+                      child: Text('Discussion'),
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 30)),
+                        maximumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)),
                         backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
                         foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Expanded(
-                  child: FittedBox(
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                      },
-                      icon: Icon(Icons.close),
-                      label: Text('Not Going'),
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                        ),
-                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
-                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
-                      ),
-                    ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                      'About Event'
                   ),
-                ),
-              ],
-            )
-          ],
+                  Text(
+                      'Lorem ipsum dolor sit amet consectetur. Volutpat ac pulvinar egestas id vitae. Risus lacinia ac aliquam bibendum viverra facilisis id. Neque nisi pharetra quis ultrices habitasse vivamus leo. Ultricies morbi in morbi non.'
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                      'Guest Speaker'
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
