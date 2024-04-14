eaf7f62df0adc8c3d2303918b9f2ea9af4069554
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-14 19:42:27 +0800

    Update events_main.dart

--- lib/screens/EventScreen/events_main.dart
+++ lib/screens/EventScreen/events_main.dart
@@ -18,76 +18,92 @@ class MyApp extends StatelessWidget {
           width: MediaQuery.of(context).size.width * 0.5,
           child: Container(
             color: Color(0xFFC7CCCA),
-            child: ListView(
+            child: Column(
               children: [
-                DrawerHeader(
-                  child: Center(
-                    child: Image.asset('assets/gthr_Logo.png'),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      DrawerHeader(
+                        child: Center(
+                          child: Image.asset('assets/gthr_Logo.png'),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.home,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Home',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        onTap: (){
+                          'Navigator.of(context).push('
+                              'MaterialPageRoute(builder: (context)=> homepage())'
+                              ')';
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.event,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Events',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.chat_bubble,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Chats',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.people_alt_rounded,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Friends',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.person,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Profile',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(
+                          Icons.settings,
+                          color: Color(0xFF1E5720),
+                        ),
+                        title: Text(
+                          'Settings',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 ListTile(
                   leading: Icon(
-                      Icons.home,
+                    Icons.logout,
                     color: Color(0xFF1E5720),
                   ),
                   title: Text(
-                    'Home',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                  onTap: (){
-                    'Navigator.of(context).push('
-                        'MaterialPageRoute(builder: (context)=> homepage())'
-                        ')';
-                  },
-                ),
-                ListTile(
-                  leading: Icon(
-                      Icons.event,
-                    color: Color(0xFF1E5720),
-                  ),
-                  title: Text(
-                    'Events',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(
-                      Icons.chat_bubble,
-                    color: Color(0xFF1E5720),
-                  ),
-                  title: Text(
-                    'Chats',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(
-                      Icons.people_alt_rounded,
-                    color: Color(0xFF1E5720),
-                  ),
-                  title: Text(
-                    'Friends',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(
-                      Icons.person,
-                    color: Color(0xFF1E5720),
-                  ),
-                  title: Text(
-                    'Profile',
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(
-                      Icons.settings,
-                    color: Color(0xFF1E5720),
-                  ),
-                  title: Text(
-                    'Settings',
-                    style: TextStyle(fontSize: 15),
+                    'Logout',
+                    style: TextStyle(fontSize: 18),
                   ),
                 ),
               ],
