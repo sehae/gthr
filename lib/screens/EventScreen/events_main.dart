ec8311fc27458ebb0d01d4f6ba92a441ebb7c4cf
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-15 10:58:01 +0800

    almost done with events ui.
    mga kulang:
    feedback, after the event screen

--- lib/screens/EventScreen/events_main.dart
+++ lib/screens/EventScreen/events_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 
 void main() {
   runApp(MyApp());
@@ -158,7 +159,16 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Content extends StatelessWidget {
+class Content extends StatefulWidget {
+  @override
+  _ContentState createState() => _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  bool showAboutContainer = true;
+  bool showDiscussionContainer = false;
+  String text = 'Lorem ipsum dolor sit amet consectetur. Volutpat ac pulvinar egestas id vitae. Risus lacinia ac aliquam bibendum viverra facilisis id. Neque nisi pharetra quis ultrices habitasse vivamus leo. Ultricies morbi in morbi non.Lorem ipsum dolor sit amet consectetur. Volutpat ac pulvinar egestas id vitae. Risus lacinia ac aliquam bibendum viverra facilisis id. Neque nisi pharetra quis ultrices habitasse vivamus leo. Ultricies morbi in morbi non.';
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -202,6 +212,7 @@ class Content extends StatelessWidget {
                         icon: Icon(Icons.star),
                         label: Text('Interested'),
                         style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 10)),
                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0),
@@ -215,7 +226,7 @@ class Content extends StatelessWidget {
                   ),
                   SizedBox(width: 10),
                   Container(
-                    width: 100,
+                    width: 97,
                     child: Expanded(
                       child: FittedBox(
                         child: ElevatedButton.icon(
@@ -224,6 +235,7 @@ class Content extends StatelessWidget {
                           icon: Icon(Icons.check),
                           label: Text('Going'),
                           style: ButtonStyle(
+                            padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 10)),
                             shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                               RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(10.0),
@@ -245,6 +257,7 @@ class Content extends StatelessWidget {
                         icon: Icon(Icons.close),
                         label: Text('Not Going'),
                         style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 10)),
                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                             RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10.0),
@@ -277,12 +290,14 @@ class Content extends StatelessWidget {
                           fontSize: 20,
                         ),
                       ),
+                      SizedBox(height: 5,),
                       Text(
                         '9 People are Interested',
                         style: TextStyle(
                           fontSize: 16,
                         ),
                       ),
+                      SizedBox(height: 5,),
                       Row(
                         children: [
                           Icon(
@@ -299,6 +314,7 @@ class Content extends StatelessWidget {
                           ),
                         ],
                       ),
+                      SizedBox(height: 5,),
                       Row(
                         children: [
                           Icon(
@@ -317,49 +333,79 @@ class Content extends StatelessWidget {
                       )
                     ],
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        width: 60,
-                        height: 80,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFD9D9D9),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                        child: Center(
-                          child: Text(
-                            'Feb 26',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color(0xFF2C2C30),
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
+                  Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            width: 70,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFD9D9D9),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                            child: Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Feb',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF2C2C30),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    '26',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF2C2C30),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      Container(
-                        width: 60,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xFFD9D9D9),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                        child: Center(
-                          child: Text(
-                            '10:30am',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Color(0xFF2C2C30),
-                              fontSize: 10,
-                              fontWeight: FontWeight.bold,
+                          SizedBox(height: 10),
+                          Container(
+                            width: 70,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFD9D9D9),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                            child: Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '10:30am',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF2C2C30),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    '11:30am',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: Color(0xFF2C2C30),
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              )
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
                   ),
                 ],
               ),
@@ -368,8 +414,18 @@ class Content extends StatelessWidget {
                 children: [
                   Expanded(
                     child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('About'),
+                      onPressed: () {
+                        setState(() {
+                          showAboutContainer = true;
+                          showDiscussionContainer = false;
+                        });
+                      },
+                      child: Text(
+                          'About',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
                       style: ButtonStyle(
                         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
@@ -378,8 +434,12 @@ class Content extends StatelessWidget {
                         ),
                         minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 30)),
                         maximumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)),
-                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFF787878)),
-                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFFFFFFFF)),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            showAboutContainer ? Color(0xFF787878) : Color(0xFFD9D9D9)
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            showAboutContainer ? Color(0xFFFFFFFF) : Color(0xFF2C2C30)
+                        ),
                       ),
                     ),
                   ),
@@ -387,8 +447,17 @@ class Content extends StatelessWidget {
                   Expanded(
                     child: ElevatedButton(
                       onPressed: () {
+                        setState(() {
+                          showDiscussionContainer = true;
+                          showAboutContainer = false;
+                        });
                       },
-                      child: Text('Discussion'),
+                      child: Text(
+                          'Discussion',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
                       style: ButtonStyle(
                         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                           RoundedRectangleBorder(
@@ -397,29 +466,93 @@ class Content extends StatelessWidget {
                         ),
                         minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 30)),
                         maximumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 50)),
-                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
-                        foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            showDiscussionContainer ? Color(0xFF787878) : Color(0xFFD9D9D9)
+                        ),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            showDiscussionContainer ? Color(0xFFFFFFFF) : Color(0xFF2C2C30)
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
               SizedBox(height: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                      'About Event'
+              if (showAboutContainer)
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          'About Event',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      ReadMoreText(
+                        text,
+                        trimLines: 5,
+                        trimMode: TrimMode.Line,
+                        trimCollapsedText: 'Read More.',
+                        trimExpandedText: 'Read Less.',
+                        moreStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1E7251),
+                          decoration: TextDecoration.underline,
+                        ),
+                        lessStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1E7251),
+                          decoration: TextDecoration.underline,
+                        ),
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                          'Guest Speaker'
+                      ),
+                    ],
                   ),
-                  Text(
-                      'Lorem ipsum dolor sit amet consectetur. Volutpat ac pulvinar egestas id vitae. Risus lacinia ac aliquam bibendum viverra facilisis id. Neque nisi pharetra quis ultrices habitasse vivamus leo. Ultricies morbi in morbi non.'
+                ),
+              if (showDiscussionContainer)
+                Container(
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: (){
+                        },
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all<EdgeInsetsGeometry>(EdgeInsets.symmetric(horizontal: 10)),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFD9D9D9)),
+                          foregroundColor: MaterialStateProperty.all<Color>(Color(0xFF2C2C30)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                                'Sort By: Recent Activity',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                            Icon(
+                                Icons.keyboard_arrow_down,
+                              size: 35,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                      'Guest Speaker'
-                  ),
-                ],
-              ),
+                )
             ],
           ),
         ),
