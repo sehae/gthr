48aa62223fcb97688b5d323bf5852c507ca974a2
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-22 21:51:22 +0800

    Create splash_screen.dart

---
+++ lib/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:gthr/initialScreens/SelectionScreen/selectionscreen.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/logowtext.png', // GTHR LOGO
+            ),
+            SizedBox(height: 100), // Button to logo spacing
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SelectionScreen(),
+                ));
+              },
+              child: Text('Get Started'),
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xff1E7251), // BG color
+                onPrimary: Colors.white, // Text color
+                minimumSize: Size(250, 50), // Button minimum size
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
