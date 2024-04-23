6ea611426c97b1ed6881f12bb40fea0c35b20dc5
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-23 10:22:57 +0800

    Create auth.dart

---
+++ lib/services/auth.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // sign in anon
+
+  // sign in with email & password
+
+  //register with email & password
+
+  // sign out
+}
