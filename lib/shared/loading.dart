fd83d205dd67a2943c0489b7b79c61a945f70944
sensehae <106131457+sehae@users.noreply.github.com>
2024-04-30 00:00:54 +0800

    Update loading.dart

--- lib/shared/loading.dart
+++ lib/shared/loading.dart
@@ -8,9 +8,9 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xffFFDD0A),
+      color: const Color(0xffFFFFFF),
       child: const Center(
-        child: SpinKitPouringHourGlass(
+        child: SpinKitRotatingPlain(
           color: Color(0xff1E7251),
           size: 50.0,
         ),
