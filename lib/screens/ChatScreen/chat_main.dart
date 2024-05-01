7397dc0f04e70faec1259a3e9fd13bb2d263e57b
Yohan <94430931+takoyakiyam@users.noreply.github.com>
2024-05-01 18:57:11 +0800

    Updated chat_main.dart

--- lib/screens/ChatScreen/chat_main.dart
+++ lib/screens/ChatScreen/chat_main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import '../../models/user_list.dart';
+import '../../services/database.dart';
 
 void main() {
   runApp(const ChatPage());
@@ -6,13 +9,6 @@ void main() {
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
-
-  //final String userEmail;
-  //final String receiverUserID;
-  //const MyApp({super.key,
-  //required this.receiverUserEmail,
-  //required this.receiverUserID,
-  //});
 
   @override
   Widget build(BuildContext context) {
@@ -35,37 +31,31 @@ class Content extends StatefulWidget {
 }
 
 class _ContentState extends State<Content> {
+  late Stream<List<UserList>> _usersStream;
   final double headerHeight = 180;
 
   int selectedIndex = 0;
 
   _ContentState() {
     selectedIndex = 0;
+    _usersStream = DatabaseService().userLists; // Assuming it fetches user data
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      body: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              buildHeader(),
-              Padding(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    if (selectedIndex == 0) buildMessagesContent(),
-                    if (selectedIndex == 1) buildGroupsContent(),
-                  ],
-                ),
-              )
-            ],
+      body: Column(
+        children: [
+          buildHeader(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(0),
+              child: selectedIndex == 0
+                  ? buildMessagesStream()
+                  : buildGroupsContent(),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -76,28 +66,20 @@ class _ContentState extends State<Content> {
       height: headerHeight,
       child: Column(
         children: [
-          Expanded(
-            child: Container(),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: buildContent(),
-          ),
+          Expanded(child: Container()),
+          buildContent(),
         ],
       ),
     );
   }
 
   Widget buildContent() {
-    return SizedBox(
-      width: double.infinity,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          buildContentButton('Messages', 0),
-          buildContentButton('Groups', 1),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        buildContentButton('Messages', 0),
+        buildContentButton('Groups', 1),
+      ],
     );
   }
 
@@ -105,65 +87,105 @@ class _ContentState extends State<Content> {
     final isActive = selectedIndex == index;
 
     return Expanded(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Container(
-            decoration: BoxDecoration(
-                border: Border(
-                    bottom: BorderSide(
-                      color: isActive ? const Color(0xFFFFDD0A) : Colors.transparent,
-                      width: 5.0,
-                    ))),
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-              style: ButtonStyle(
-                overlayColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return Colors.transparent;
-                    }
-                    return Colors.transparent;
-                  },
-                ),
-                foregroundColor: MaterialStateProperty.resolveWith<Color>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.pressed)) {
-                      return Colors.white.withOpacity(0.8);
-                    }
-                    return isActive ? Colors.white : Colors.white;
-                  },
-                ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              bottom: BorderSide(
+                color: isActive ? const Color(0xFFFFDD0A) : Colors.transparent,
+                width: 5,
               ),
-              child: Text(text,
-                  style: const TextStyle(
-                    fontSize: 18,
-                  )),
             ),
           ),
-        ));
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith(
+                (states) =>
+                    isActive ? Colors.white : Colors.white.withOpacity(0.8),
+              ),
+              overlayColor: MaterialStateProperty.all(Colors.transparent),
+            ),
+            child: Text(
+              text,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  Widget buildMessagesContent() {
-    return const Column(
-      children: [
-        Center(
-          child: Text('Loading messages...'),
-        ),
-      ],
+  Widget buildMessagesStream() {
+    return StreamBuilder<List<UserList>>(
+      stream: _usersStream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          return buildMessagesContent(snapshot.data!);
+        } else {
+          return const Center(child: Text('No data available'));
+        }
+      },
+    );
+  }
+
+  Widget buildMessagesContent(List<UserList> users) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: users.length, // This should be the number of users in the list
+      itemBuilder: (context, index) {
+        final user = users[index]; // Extract the user from the list
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 15),
+          child: ListTile(
+              title: Text(
+                '${user.fname} ${user.lname}', // Display user's full name
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              subtitle: const Text(
+                'Most recent chat message',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundColor: const Color(0xFF1E7251),
+                backgroundImage: user.icon != null
+                    ? MemoryImage(base64Decode(user.icon))
+                    : null,
+                child: (user.icon == null || user.icon.isEmpty)
+                    ? Text(
+                        user.fname[0].toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      )
+                    : null,
+              ),
+              onTap: () {}),
+        );
+      },
     );
   }
 
   Widget buildGroupsContent() {
-    return const Column(
-      children: [
-        Center(
-          child: Text('Loading groups...'),
-        ),
-      ],
+    return const Center(
+      child: Text('You currently have no groups'),
     );
   }
 }
