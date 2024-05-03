546e4661a8af6bddd11c91859eaa46ba5951df09
sensehae <106131457+sehae@users.noreply.github.com>
2024-05-03 22:47:01 +0800

    added another class 4 the reply function

--- lib/models/user_posts.dart
+++ lib/models/user_posts.dart
@@ -11,3 +11,17 @@ class Post {
     this.isEdited = false,
   });
 }
+
+class Reply {
+  final String content;
+  final DateTime timestamp;
+  final String postId;
+  final String userId;
+
+  Reply({
+    required this.content,
+    required this.timestamp,
+    required this.postId,
+    required this.userId,
+  });
+}
