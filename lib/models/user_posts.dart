2ca20d787e49afc633d58cc129c1854b72f22868
sensehae <106131457+sehae@users.noreply.github.com>
2024-05-03 22:05:01 +0800

    added bool to check if the post is edited or not

--- lib/models/user_posts.dart
+++ lib/models/user_posts.dart
@@ -2,10 +2,12 @@ class Post {
   final String content;
   final DateTime timestamp;
   final String? postId;
+  final bool isEdited;
 
   Post({
     required this.content,
     required this.timestamp,
     this.postId,
+    this.isEdited = false,
   });
 }
